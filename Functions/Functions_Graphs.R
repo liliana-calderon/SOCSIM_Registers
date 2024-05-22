5839a508e8310b44cff3db35a5e4bc98ee3618af
Calderon Bernal <calderonbernal@demogr.mpg.de>
2024-05-22 14:48:49 +0200

    Add theme for the graphs as a function and increase the text size

--- Functions/Functions_Graphs.R
+++ Functions/Functions_Graphs.R
@@ -41,6 +41,26 @@ theme_graphs <- function() {
   )
 }
 
+## Creation of a theme for the comparison graphs
+
+theme_graphs2 <- function() {
+  theme(panel.grid.major.y = element_line(colour = "#999999", linewidth = 0.3, linetype = 9),
+        panel.grid.major.x = element_line(colour = "#999999", linewidth = 0.3, linetype = 9),
+        panel.grid.minor.x = element_line(colour = "#999999", linewidth = 0.3, linetype = 9),
+        panel.grid.minor = element_blank(), 
+        legend.position = "bottom",
+        panel.background = element_rect(fill = NA), 
+        panel.ontop = TRUE,
+        text = element_text(family = "serif", size = 12),
+        legend.key = element_blank(),
+        strip.background = element_rect(color = "#000000", fill = "#FFFFFF"),
+        strip.text = element_text(family="serif", face="bold", size=26),
+        axis.title.x = element_text(family="serif", size = 22, colour = "#000000", 
+                                    hjust=0.5, face = "bold", margin = margin(10, 0, 0, 0)), 
+        axis.title.y = element_text(family="serif", size = 22, colour = "#000000", face = "bold"), 
+        axis.text = element_text(family="serif", size = 16, colour = "#000000"),
+        legend.text = element_text(family="serif", size = 18, colour = "#000000")) }
+
 # Convert SOCSIM months to calendar years. 
 asYr <- function(month, last_month, final_sim_year) {
   return(final_sim_year - trunc((last_month - month)/12))
